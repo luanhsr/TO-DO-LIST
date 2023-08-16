c5ffca812820f8e40495427b2cc299001fa658b1
luanhsr <107119057+luanhsr@users.noreply.github.com>
2023-08-16 14:26:36 -0300

    0.0.1.2
    criando novo card.

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
 window.onload = () => {
     const newCardButton = document.getElementById("new-card-btn");
     newCardButton.addEventListener('click', () => {
-        console.log('Novo card clicado');
+        let createCard = document.createElement('div');
+        let idRandon = Math.random();
+        createCard.setAttribute('id' , 'card' + idRandon);
+        createCard.setAttribute('class' , card); // para receber todos os atributos CSS ja criados no cartao padrao.
+        createCard.setAttribute('draggable' , 'true');
+        createCard.setAttribute('ondragstart' , 'drag(event)');
+        let createCardHead = document.createElement('div');
+        createCardHead.setAttribute('class' , 'head-card');
+        let createCardH4 = document.createElement('h4');
+        createCardH4.innerHTML = 'Insira o nome da tarefa clicando no icone de editar'
+        let createCardButtonEdit = document.createElement('button');
+        createCardButtonEdit.setAttribute('id' , );
+        
+        
+
+        /*
+
+                <button id="edit-card" class="edit-card"><span class="material-symbols-outlined"> edit  </span></button>
+            </div>
+            <div class="body-card">
+                 <p class="desc">Clique no icone de edicao para editar sua tarefa</p>
+            <span class="date-created-task">tarefa criada em: 09/08/2023 </span>
+            </div>
+           
+        </div> */
     });
 
     const btnEditC = document.getElementById("edit-card");
@@ -10,7 +34,7 @@ window.onload = () => {
     if (parentCard) {
         const parentId = parentCard.id;
         let cardEdit =  document.getElementById(parentId);
-        let editableElements = cardEdit.querySelectorAll('p, h4' );
+        let editableElements = cardEdit.querySelectorAll('P, h4' );
         editableElements.forEach(element => {
             if (element.tagName == 'P' || element.tagName =='H4') {
                 element.setAttribute('contenteditable', 'true'); 
