649eaa8af212355c3d1f31fc5a53fe7be14caa2e
luanhsr <107119057+luanhsr@users.noreply.github.com>
2023-08-16 20:29:01 -0300

    Adicionar tarefa cartão 0.0.1.3
    Adiciona o novo cartão mas ainda não está pronto.

--- index.js
+++ index.js
@@ -1,36 +1,86 @@
 window.onload = () => {
     const newCardButton = document.getElementById("new-card-btn");
+    const taskCreatedContainer = document.getElementById("task-created"); 
     newCardButton.addEventListener('click', () => {
-        let createCard = document.createElement('div');
         let idRandon = Math.random();
-        createCard.setAttribute('id' , 'card' + idRandon);
-        createCard.setAttribute('class' , card); // para receber todos os atributos CSS ja criados no cartao padrao.
-        createCard.setAttribute('draggable' , 'true');
-        createCard.setAttribute('ondragstart' , 'drag(event)');
+
+        // Criar o elemento div para o card
+        let createCard = document.createElement('div');
+        createCard.setAttribute('id', 'card' + idRandon);
+        createCard.setAttribute('class', 'card');
+        createCard.setAttribute('draggable', 'true');
+        createCard.setAttribute('ondragstart', 'drag(event)');
+    
+        // Criar a estrutura do card
+        // INICIO header do card -----------------------------------------------------
         let createCardHead = document.createElement('div');
-        createCardHead.setAttribute('class' , 'head-card');
+        createCardHead.setAttribute('class', 'head-card');
+        // FIM header do card ----------------------------------------------------------
+        
+        // INICIO titulo do card --------------------------------------------------------
         let createCardH4 = document.createElement('h4');
-        createCardH4.innerHTML = 'Insira o nome da tarefa clicando no icone de editar'
+        createCardH4.innerHTML = 'Insira o nome da tarefa clicando no ícone de editar';
+        createCardHead.appendChild(createCardH4);
+        // FINAL  titulo do car------------------------------------------------------
+
+        // INICIO Botão editar do card -------------------------------------------
         let createCardButtonEdit = document.createElement('button');
-        createCardButtonEdit.setAttribute('id' , );
+        createCardButtonEdit.setAttribute('id', 'edit-card');
+        createCardButtonEdit.setAttribute('class', 'edit-card');
+        // FINAL botão editar do card ------------------------------------------------
+
+        // INICIO icone google span do card -------------------------------------------
+        let createSpanI = document.createElement('span');
+        createSpanI.setAttribute('class', 'material-symbols-outlined');
+        createSpanI.textContent = 'edit';
+        // FINAL  icone google span do card -------------------------------------------
+
+        // INICIO corpo do card -----------------------------------------------
+        let createCardBody = document.createElement('div');
+        createCardBody.getAttribute('class' , 'body-card');
+        // Final corpo do card -----------------------------------------------
+
+        // INICIO descrição do card -----------------------------------------------
+        let createPCard = document.createElement('p');
+        createPCard.getAttribute('class' , 'desc');
+        createPCard.innerHTML = 'Clique no botão de editar para inserir a descrição da sua tarefa aqui';
+        createCardBody.appendChild(createPCard);
+        // FIM descrição do card ---------------------------------------------------
+
+        // Adicionar a estrutura do card ao card
+                /*
+         <div id="card"class="card" draggable="true" ondragstart="drag(event)">
+                        <div class="head-card">
+                            <h4> Primeira tarefa</h4> 
+                            <button id="edit-card" class="edit-card"><span class="material-symbols-outlined"> edit  </span></button>
+                        </div>
+                        <div class="body-card">
+                             <p class="desc">Clique no icone de edicao para editar sua tarefa</p>
+                        <span class="date-created-task">tarefa criada em: 09/08/2023 </span>
+                        </div>
+                       
+                    </div>
+        */         
+        createCard.appendChild(createCardHead);
+        createCard.appendChild(createCardBody);
+        createCardHead.appendChild(createCardButtonEdit)
+        createCardButtonEdit.appendChild(createSpanI);
+    
         
         
-
-        /*
-
-                <button id="edit-card" class="edit-card"><span class="material-symbols-outlined"> edit  </span></button>
-            </div>
-            <div class="body-card">
-                 <p class="desc">Clique no icone de edicao para editar sua tarefa</p>
-            <span class="date-created-task">tarefa criada em: 09/08/2023 </span>
-            </div>
-           
-        </div> */
+    
+        // Adicionar o novo card ao container adequado no DOM
+        // Suponhamos que o container tenha o ID "card-container"
+        taskCreatedContainer.appendChild(createCard);
+        
+   
+        
     });
 
     const btnEditC = document.getElementById("edit-card");
     btnEditC.addEventListener('click', function() {
     const parentCard = this.closest('.card');
+    console.log(parentCard)
     if (parentCard) {
         const parentId = parentCard.id;
         let cardEdit =  document.getElementById(parentId);
