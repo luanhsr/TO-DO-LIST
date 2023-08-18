0910312d9cbb2ece7db53efc61627b0db15f1784
luanhsr <107119057+luanhsr@users.noreply.github.com>
2023-08-18 20:24:48 -0300

    Botão de editar/salvar
    Criando a UX para o usuário editar e salvar um card.
    Ao clicar no botão de editar o botão de editar se torna um botão de salvar, indicando que o usuário deve clicar novamente para salvar as alterações.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ window.onload = () => {
     const newCardButton = document.getElementById("new-card-btn");
     const taskCreatedContainer = document.getElementById("task-created"); 
     newCardButton.addEventListener('click', () => {
+       
         let idRandon = Math.random();
 
         // Criar o elemento div para o card
@@ -37,36 +38,22 @@ window.onload = () => {
 
         // INICIO corpo do card -----------------------------------------------
         let createCardBody = document.createElement('div');
-        createCardBody.getAttribute('class' , 'body-card');
+        createCardBody.setAttribute('class' , 'body-card');
         // Final corpo do card -----------------------------------------------
 
         // INICIO descrição do card -----------------------------------------------
         let createPCard = document.createElement('p');
-        createPCard.getAttribute('class' , 'desc');
+        createPCard.setAttribute('class' , 'desc');
         createPCard.innerHTML = 'Clique no botão de editar para inserir a descrição da sua tarefa aqui';
         // FIM descrição do card ---------------------------------------------------
 
         // INICIO data criação do card ---------------------------------------------------------
         
         let createSpanCardDate = document.createElement('span');
-        createSpanCardDate.getAttribute('class' , 'date-created-task');
+        createSpanCardDate.setAttribute('class' , 'date-created-task');
         createSpanCardDate.innerHTML = 'data aleatoria 16/08/2023'
-        
 
-        // Adicionar a estrutura do card ao card
-                /*
-         <div id="card"class="card" draggable="true" ondragstart="drag(event)">
-                        <div class="head-card">
-                            <h4> Primeira tarefa</h4> 
-                            <button id="edit-card" class="edit-card"><span class="material-symbols-outlined"> edit  </span></button>
-                        </div>
-                        <div class="body-card">
-                             <p class="desc">Clique no icone de edicao para editar sua tarefa</p>
-                        <span class="date-created-task">tarefa criada em: 09/08/2023 </span>
-                        </div>
-                       
-                    </div>
-        */      
+        // inserindo todos os elementos criados, montando o corpo do cartão.
         taskCreatedContainer.appendChild(createCard);   
         createCard.appendChild(createCardHead);
         createCardHead.appendChild(createCardH4);
@@ -75,29 +62,41 @@ window.onload = () => {
         createCard.appendChild(createCardBody);
         createCardBody.appendChild(createPCard);
         createCardBody.appendChild(createSpanCardDate);
-                
-        // Adicionar o novo card ao container adequado no DOM
-        // Suponhamos que o container tenha o ID "card-container"
-        
         
     });
 
     const btnEditC = document.getElementById("edit-card");
+    const btnSave = document.getElementById("save-card");
     btnEditC.addEventListener('click', function() {
-    const parentCard = this.closest('.card');
-    if (parentCard) {
-        const parentId = parentCard.id;
-        let cardEdit =  document.getElementById(parentId);
-        let editableElements = cardEdit.querySelectorAll('P, h4' );
-        editableElements.forEach(element => {
-            if (element.tagName == 'P' || element.tagName =='H4') {
-                element.setAttribute('contenteditable', 'true'); 
+        let displayVal = btnEditC.style.display; // variável recebe o display do elemento, para verificar se está padrão ou none
+        if (displayVal ==  'block') { // se estiver block (padrão) quer dizer que está visível então poderá realizar as instruções
+            const parentCard = this.closest('.card'); // pega o elemento parente pai clicado.
+            if (parentCard) {
+                const parentId = parentCard.id; // pega o id desse elemento pai 
+                let cardEdit =  document.getElementById(parentId); // pega o elemento pai selecionado no documento.
+                let editableElements = cardEdit.querySelectorAll('P, h4' ); // pega todos os elementos P e H4 e salva em um array
+                editableElements.forEach(element => { // faz uma busca em todos os elementos desse Array
+                    if (element.tagName == 'P' || element.tagName =='H4') { // se os elementos forem Parágrafo ou Titulo H4
+                        element.setAttribute('contenteditable', 'true');   // recebe o atributo citado os tornando editáveis.
+                    }
+                }); 
             }
-        });
-    }   
-});
+            btnEditC.style.display = 'none'; // transforma o botão de editar com o estilo 'none'
+            btnSave.style.display = 'block'; // transforma o botão salvar com o estilo block
+            // um some o outro aparece.
+        } 
+    });
+    btnSave.addEventListener('click', function() { // ao clicar
+        let displayVal = btnSave.style.display; // pega o atributo display do botão save
+        if (displayVal == 'block') { // verifica se está block 
+            btnEditC.style.display = 'block'; // torna o botão EDIT visível.
+            btnSave.style.display = 'none'; // torna o botão save invisível.
+        }
+    });
 
 };
+
+// drag e drop dos cartões: 
 function allowDrop(event) {
     event.preventDefault();
 }
@@ -112,3 +111,4 @@ function drop(event) {
     const draggedElement = document.getElementById(data);
     event.target.appendChild(draggedElement);
 }
+// fim drag drop dos cartõe
