89c60bbf7b2b053383ab85c43010e32c3e1225eb
luanhsr <107119057+luanhsr@users.noreply.github.com>
2023-09-30 15:30:15 -0300

    0.1.0.0.5 melhorando a ux no cadastro
    Melhorando a ux no cadastro deixando os ícones mostrarem quando o usuário está digitando os campos corretamente, mostrando um ícone de erro e acerto caso ele esteja errado junto a uma mensagem do erro.

--- assets/JavaScript/cadastro.js
+++ assets/JavaScript/cadastro.js
@@ -1,99 +1,50 @@
-const bdUserTemp = [];
-const user = new Object();
-document.getElementById('getUser').addEventListener('click' , ()=> {
- event.preventDefault();
-
-    var email = document.getElementById('email').value;
-    var verifyE = document.getElementById('confirm-email').value;
-    var password = document.getElementById('password').value;
-    var verifyP = document.getElementById('confirm-password').value;
-    const correction = document.getElementById('correction')
-    const erroremail = document.getElementById('erroremail');
-    const errorconfirmemail = document.getElementById('errorconfirmemail');
-    const errorpassword = document.getElementById('errorpassword');
-    const errorconfirmpassword = document.getElementById('errorconfirmpassword');
-
-
-   
-    if (email.length === 0) {
-        correction.innerHTML = 'O email deve ser preenchido!';
-        correction.style.color = 'red';
-        erroremail.style.visibility = 'visible'
-    } else if (!validateEmail(email)) {
-        correction.innerHTML = 'Precisa ser um email valido!';
-        correction.style.color = 'red';
-        erroremail.style.visibility = 'visible'
-    } else if (verifyE.length === 0) {
-        correction.innerHTML = 'A verificacao de e-mail deve ser preenchido!';
-        correction.style.color = 'red';
-        errorconfirmemail.style.visibility = 'visible'
-    } else if (email !== verifyE) {
-        correction.innerHTML = 'O email nao coincide! por favor preencha novamente.';
-        correction.style.color = 'red';
-        erroremail.style.visibility = 'visible'
-        errorconfirmemail.style.visibility = 'visible'
-    } else if ( password.length === 0) {
-        correction.innerHTML = 'A senha deve ser preenchida!';
-        correction.style.color = 'red';
-        errorpassword.style.visibility = 'visible'
-    } else if ( verifyP.length === 0) {
-        correction.innerHTML = 'A verificacao de senha deve ser preenchida!';
-        correction.style.color = 'red';
-        errorconfirmpassword.style.visibility = 'visible'
-    } else if (password.length < 4 ) {
-        correction.innerHTML = 'Sua senha tem que ter mais de 4 digitos, proteja suas tarefinhas ☺️';
-        correction.style.color = 'red';
-        errorpassword.style.visibility = 'visible'
-    } 
-    else if (password !== verifyP) {
-        correction.innerHTML = 'A senha nao coincide! por favor preencha novamente.';
-        correction.style.color = 'red';
-        errorpassword.style.visibility = 'visible'
-        errorconfirmpassword.style.visibility = 'visible'
-    }
-     else {
-        
-        correction.innerHTML = 'Usario Cadastrado';
-        correction.style.color = 'black';
-        document.getElementById('email').value = '';
-        document.getElementById('confirm-email').value = '';
-        document.getElementById('password').value = '';
-        document.getElementById('confirm-password').value = '';
-
-        
-        const newUserObj = {
-            uEmail: email,
-            UPassword: password,
-            id: Math.random()
-        };
-        bdUserTemp.push(newUserObj);
-        console.log('cadastrado com sucesso');
-        console.log(bdUserTemp);
-    }
-
-});
-function validateEmail(email) {
-    var re = /\S+@\S+\.\S+/;
-    return re.test(email);
-    /* verifica se tem Qualquer tipo de string;
-    Seguida por um caractere @ (que é obrigatório em e-mails);
-    Seguido por algum outro texto, o domínio/provedor;
-    E então temos a presença de um ponto, que também é obrigatório;
-    E por fim mais um texto, validando tanto emails .com quanto .com.br, e outros que tenham terminologias diferentes*/
+function controlIcon(id, visibility) {
+    const element = document.getElementById(id);
+    element.style.visibility = visibility;
 }
-// quando o usuario comeca a digitar no input o icone volta a ficar oculto
+function validateEmail(email) {
+    var re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return re.test(email);
+     /* verifica se tem Qualquer tipo de string;
+        Seguida por um caractere @ (que é obrigatório em e-mails);
+        Seguido por algum outro texto, o domínio/provedor;
+        E então temos a presença de um ponto, que também é obrigatório;
+        E por fim mais um texto, validando tanto emails .com quanto .com.br, e outros que tenham terminologias diferentes*/
+}
+function verifyEmail (email) {
+    let correction = document.getElementById('correction');
+    if (email.length ===0 ){
+        correction.innerHTML === `'O campo ${nameInput} deve ser preenchido'`;
+        controlIcon(error , 'visible');
+        controlIcon(done  , 'hidden');
+    } else if (validateEmail(email) == false) {
+        correction.innerHTML = `O campo ${nameInput} esta incompleto`
+        correction.style.color = 'red';
+        controlIcon(error , 'visible');
+        controlIcon(done  , 'hidden');
+    } else if(validateEmail(email) == true) {
+        correction.innerHTML = `${nameInput} completo!`;
+        correction.style.color = 'green';
+        controlIcon(done , 'visible');
+        controlIcon(error  , 'hidden');
+        const checked1 = true;
+    }
+}
 document.getElementById('email').addEventListener('input', function() {
-    erroremail.style.visibility = 'hidden';
+    error = 'erroremail';
+    done = 'doneemail';
+    nameInput = 'Email';
+    email = this.value;
+    verifyEmail(this.value);
+  
+
 });
 
-document.getElementById('confirm-email').addEventListener('input', function() {
-    errorconfirmemail.style.visibility = 'hidden';
-});
+document.getElementById('confirmemail').addEventListener('input', function() {
+    error = 'errorconfirmemail';
+    done = 'doneconfirmemail';
+    nameInput = 'Email confirmação';
+    verifyEmail(this.value);
+    email = this.value;
 
-document.getElementById('password').addEventListener('input', function() {
-    errorpassword.style.visibility = 'hidden';
-});
-
-document.getElementById('confirm-password').addEventListener('input', function() {
-    errorconfirmpassword.style.visibility = 'hidden';
 });
