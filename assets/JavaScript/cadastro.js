f46a43e7cbbaa306379d38b46f059687bcd29c39
luanhsr <107119057+luanhsr@users.noreply.github.com>
2023-10-02 17:35:37 -0300

    0.1.0.0.6 melhorando a ux no cadastro
    Os ícones e as mensagens de erro se atualizam conforme o erro específico do usuário, a verificação e feito por uma escuta no input para que antes mesmo do usuário enviar os dados ele saber que está tudo certo.

--- assets/JavaScript/cadastro.js
+++ assets/JavaScript/cadastro.js
@@ -1,9 +1,31 @@
-function controlIcon(id, visibility) {
-    const element = document.getElementById(id);
-    element.style.visibility = visibility;
+const correction = document.getElementById('correction');
+var checkedEmail = false;
+var checkedCEmail = false;
+var checkedPassword = false;
+var checkedCPassword = false;
+var okEmail = false;
+var okPassowrd = false;
+function mesageCorrection (type ,inputname , messsage){
+    if (type == 'error') {
+        correction.style.color = 'red';
+    } else if (type =='done') {
+        correction.style.color = 'green';
+    }
+        correction.innerHTML = `${inputname}${messsage}`;
 }
-function validateEmail(email) {
-    var re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+function controlIcon(type, id , id2) {
+    let iconErr = document.getElementById(id);
+    let iconDone = document.getElementById(id2);
+    if (type == 'error') {
+        iconErr.style.visibility = 'visible';
+        iconDone.style.visibility = 'hidden';
+    } else if (type == 'done') {
+        iconErr.style.visibility = 'hidden';
+        iconDone.style.visibility = 'visible';
+    }
+}
+function isEmail(email) {
+    let re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return re.test(email);
      /* verifica se tem Qualquer tipo de string;
         Seguida por um caractere @ (que é obrigatório em e-mails);
@@ -11,40 +33,99 @@ function validateEmail(email) {
         E então temos a presença de um ponto, que também é obrigatório;
         E por fim mais um texto, validando tanto emails .com quanto .com.br, e outros que tenham terminologias diferentes*/
 }
-function verifyEmail (email) {
-    let correction = document.getElementById('correction');
-    if (email.length ===0 ){
-        correction.innerHTML === `'O campo ${nameInput} deve ser preenchido'`;
-        controlIcon(error , 'visible');
-        controlIcon(done  , 'hidden');
-    } else if (validateEmail(email) == false) {
-        correction.innerHTML = `O campo ${nameInput} esta incompleto`
-        correction.style.color = 'red';
-        controlIcon(error , 'visible');
-        controlIcon(done  , 'hidden');
-    } else if(validateEmail(email) == true) {
-        correction.innerHTML = `${nameInput} completo!`;
-        correction.style.color = 'green';
-        controlIcon(done , 'visible');
-        controlIcon(error  , 'hidden');
-        const checked1 = true;
-    }
+function itASequel(password) {
+    let re = /(123|234|345|456|567|678|789|8910|1098|987|876|765|654|543|432|321)/;
+    return re.test(password);
+    /*
+    verifica na senha se tem uma sequencia de tres digitos
+    */
 }
+function verifyEmail (email) {
+    let checked1 = false;
+    if (email.length ===0 ){
+        mesageCorrection ('error ' , inputname , ' nao pode estar vazio');
+        controlIcon ('error' , idErr , idDone);
+    } else if (isEmail(email) == false) {
+        mesageCorrection('error' , inputname, ' esta incompleta precisa ser um email valido');
+
+        controlIcon ('error' , idErr , idDone);
+    } else if(isEmail(email) == true) {
+        mesageCorrection ('done' , inputname, ' é válido');
+        controlIcon ('done' , idErr , idDone);
+        checked1 = true;
+    }
+     return checked1;
+}
+function verifyPassword (password) {
+    let checked1 = false;
+    if (password.length ===0) {
+        controlIcon('error' , idErr , idDone);
+        mesageCorrection ('error' , inputname, ' Nao pode ser vazio');
+    } else if (password.length < 4 ) {
+        mesageCorrection ('error' , inputname, ' tem que ter ao menos 4 digitos');
+        controlIcon('error' , idErr , idDone);
+    } else if (itASequel(password) == true) {
+        mesageCorrection ('error' , inputname, ' Nao pode ter uma sequencia de 3 numeros');
+        controlIcon('error' , idErr , idDone);
+    } else {
+        mesageCorrection ('done' , inputname, ' é válido!');
+        controlIcon('done' , idErr , idDone);
+        checked1 = true;
+    }
+    return checked1;
+
+}
+function compareValues(value1, value2) {
+    var inputValue1 = document.getElementById(value1).value;
+    var inputValue2 = document.getElementById(value2).value;
+    return inputValue1 == inputValue2;
+}
+function elementsHTML (iconid1 , iconid2 , nameinput) {
+    idErr = iconid1;
+    idDone = iconid2;
+    inputname = nameinput;
+}   
 document.getElementById('email').addEventListener('input', function() {
-    error = 'erroremail';
-    done = 'doneemail';
-    nameInput = 'Email';
-    email = this.value;
-    verifyEmail(this.value);
-  
-
+    elementsHTML ('erroremail' , 'doneemail' , 'email');
+    checkedEmail = verifyEmail(this.value);
 });
-
 document.getElementById('confirmemail').addEventListener('input', function() {
-    error = 'errorconfirmemail';
-    done = 'doneconfirmemail';
-    nameInput = 'Email confirmação';
-    verifyEmail(this.value);
-    email = this.value;
+    elementsHTML ('errorconfirmemail' , 'doneconfirmemail' , 'confirmacao de email');
+    checkedCEmail = verifyEmail(this.value);
+    if (checkedCEmail == true ){
+        if (compareValues('email' , 'confirmemail') === true ) {
+            mesageCorrection('done' , 'Os campos Emails' , ' estao corretos');;
+            okEmail = true;
+            controlIcon('done', 'errorconfirmemail' , 'doneconfirmemail');
+            console.log(okEmail);
 
+        } else {
+            mesageCorrection('error' , 'Os e-mails' , ' estao diferentes');
+            controlIcon('error', 'errorconfirmemail' , 'doneconfirmemail');
+            okEmail = false;
+            console.log(okEmail);
+        }
+    }
+});
+document.getElementById('password').addEventListener('input', function() {
+    elementsHTML ('errorpassword' , 'donepassword' , 'senha');
+    verifyPassword(this.value);
+});
+document.getElementById('confirm-password').addEventListener('input', function() {
+    elementsHTML ('errorconfirmpassword' , 'doneconfirmpassword' , 'confirmação de senha');
+    checkedCPassword = verifyPassword(this.value);
+    console.log(checkedCPassword)
+    if (checkedCPassword == true ){
+        if (compareValues('password' , 'confirm-password') === true ) {
+            mesageCorrection('done' , 'As senhas' , ' estao iguais');;
+            okPassowrd = true;
+            controlIcon('done', 'errorconfirmpassword' , 'doneconfirmpassword');
+            console.log(okPassowrd)
+        } else {
+            mesageCorrection('error' , 'As senhas' , ' estao difrentes');
+            controlIcon('error', 'errorconfirmpassword' , 'doneconfirmpassword');
+            okPassowrd = false;
+            console.log(okPassowrd)
+        }
+    }
 });
